dcaf2453b578516e3f3f9d7cb52d610cee352005
Gantulga <tady_mon@yahoo.com>
2020-09-24 13:54:16 +0800

    Updates

---
+++ precache-manifest.162bd0cc295447d8f63f9a222edeb268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803aa125d5ba1fab6759a598bb41bca3",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "c8600fede6c0268c3fc0",
+    "url": "/burger/static/css/main.ebd7ed2f.chunk.css"
+  },
+  {
+    "revision": "098ffa23a045787b9c0d",
+    "url": "/burger/static/js/2.685a7267.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/burger/static/js/2.685a7267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8600fede6c0268c3fc0",
+    "url": "/burger/static/js/main.e3834df4.chunk.js"
+  },
+  {
+    "revision": "b70cca1f39cb2c1dd795",
+    "url": "/burger/static/js/runtime-main.95ddc443.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
